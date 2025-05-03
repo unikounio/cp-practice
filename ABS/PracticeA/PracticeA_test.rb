b04a749f961b7b80cbdbf33bc84a6f6b26203898
unikounio <bubbbble428@gmail.com>
2025-05-03 21:14:38 +0900

    PracticeAのテストを作成

---
+++ ABS/PracticeA/PracticeA_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'stringio'
+require_relative './PracticeA'
+
+class PracticeATest < Minitest::Test
+  def assert_practice_a_output(input, expected)
+    $stdin = StringIO.new(input)
+    output = StringIO.new
+    $stdout = output
+    
+    begin
+      practice_a
+    ensure
+      $stdout = STDOUT
+    end
+
+    assert_equal expected, output.string
+  end
+
+  def test_case1
+    input = <<~TEXT
+      1
+      2 3
+      test
+      TEXT
+
+    expected = '6 test'
+
+    assert_practice_a_output(input, expected)
+  end
+
+  def test_case2
+    input = <<~TEXT
+      72
+      128 256
+      myonmyon
+      TEXT
+
+    expected = '456 myonmyon'
+
+    assert_practice_a_output(input, expected)
+  end
+end
