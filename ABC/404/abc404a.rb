7badc89b37c939d3b0a8cde99e3a2a30a9e37292
unikounio <bubbbble428@gmail.com>
2025-05-17 13:04:49 +0900

    ABC404Aの解答を追加し、テストを修正

---
+++ ABC/404/abc404a.rb
@@ -0,0 +1,10 @@
+def abc404a(input)
+  s = input.chomp
+  alphabets = "abcdefghijklmnopqrstuvwxyz".delete(s)
+  alphabets[0]
+end
+
+# 提出用
+  # s = gets.chomp
+  # alphabets = "abcdefghijklmnopqrstuvwxyz".delete(s)
+  # puts alphabets[0]
--- ABC/404/abc404a_test.rb
+++ ABC/404/abc404a_test.rb
@@ -4,22 +4,22 @@ require_relative 'abc404a'
 class Abc404aTest < Minitest::Test
   def test_case1
     input = "a\n"
-    expected = "d"
+    expected = "abcdefghijklmnopqrstuvwxyz".delete(input)
 
-    assert_equal expected, abc404a(input)
+    assert expected.include? abc404a(input)
   end
 
   def test_case2
     input = "abcdfhijklmnopqrstuvwxyz\n"
-    expected = "e"
+    expected = "abcdefghijklmnopqrstuvwxyz".delete(input)
 
-    assert_equal expected, abc404a(input)
+    assert expected.include? abc404a(input)
   end
 
   def test_case3
     input = "qazplwsxokmedcijnrfvuhbgt\n"
-    expected = "y"
+    expected = "abcdefghijklmnopqrstuvwxyz".delete(input)
 
-    assert_equal expected, abc404a(input)
+    assert expected.include? abc404a(input)
   end
 end
